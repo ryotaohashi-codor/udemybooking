2b1d49bafbf423fd778620d5104f166607a50b9a
ryotaohashi-codor <ohashi@codor.co.jp>
2022-11-10 14:12:02 +0900

    by lecture 41

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,27 @@ import (
 	"myapp/pkg/handlers"
 	"myapp/pkg/render"
 	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 var portNumber = ":8080"
+var app config.AppConfig
+var session *scs.SessionManager
 
 func main(){
-	var app config.AppConfig
+
+	app.InProduction = false
+
+	session := scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	app.Session = session
+
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil{
